4613dfc9c6fcbe99176681896eee09d2542896b3
xiao-kiwi <17098047772@163.com>
2020-03-15 23:00:12 +0800

    add the cookies-anti.py

---
+++ cookies-anti.py
@@ -0,0 +1,16 @@
+import requests
+from parsel import Selector
+import pprint
+
+url = 'http://www.porters.vip/verify/cookie/index.html'
+headers = {
+    "User-Agent" : "Postman"
+}
+resp = requests.get(url, headers=headers)
+print(resp.status_code)
+if resp.status_code == 200:
+    sel = Selector(resp.text)
+    res = sel.css('.list-group-item::text').extract()
+    pprint.pprint(res)
+else:
+    print("失败")
